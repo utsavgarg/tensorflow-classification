2476c140edbe476822dd2c0e77d54d12cdef545d
Utsav Garg <utsav1996@ymail.com>
2017-04-19 17:47:34 +0530

    Update download_weights.sh

--- weights/download_weights.sh
+++ weights/download_weights.sh
@@ -2,13 +2,13 @@
 wget https://www.dropbox.com/s/b96tfz2hnthfegm/vgg_f.npy
 
 #download weights for caffenet
-https://www.dropbox.com/s/8evo8ydl1jl03z4/caffenet.npy
+wget https://www.dropbox.com/s/8evo8ydl1jl03z4/caffenet.npy
 
 #download weights for vgg-16
 wget https://www.dropbox.com/s/zpeufcwesimhvua/vgg16.npy
 
 #download weights for vgg-19
-https://www.dropbox.com/s/e1v93adr4igwuct/vgg19.npy
+wget https://www.dropbox.com/s/e1v93adr4igwuct/vgg19.npy
 
 #download weights for googlenet
 wget https://www.dropbox.com/s/kzlgksuginkatb5/googlenet.npy
