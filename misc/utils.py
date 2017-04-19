930fa0c7a00c447e33ba945237b768a591a155d7
Utsav Garg <utsav1996@ymail.com>
2017-04-19 16:11:01 +0530

    added support for caffenet

--- misc/utils.py
+++ misc/utils.py
@@ -3,15 +3,15 @@ from skimage.io import imread
 from skimage.transform import resize
 
 #util layers
-def img_preprocess(img_path):
+def img_preprocess(img_path, size=224):
     mean = [103.939, 116.779, 123.68]
     img = imread(img_path)
-    img = resize(img, (224, 224))*255.0
+    img = resize(img, (size, size))*255.0
     if len(img.shape) == 2:
         img = np.dstack([img,img,img])
     img[:,:,0] -= mean[2]
     img[:,:,1] -= mean[1]
     img[:,:,2] -= mean[0]
     img[:,:,[0,1,2]] = img[:,:,[2,1,0]]
-    img = np.reshape(img,[1,224,224,3])
+    img = np.reshape(img,[1,size,size,3])
     return img
